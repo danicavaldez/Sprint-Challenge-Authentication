1029cd6d5fff9eba64cd32ddd1144adfea56a5b9
Danica Valdez <danicaevaldez@gmail.com>
2020-02-23 17:42:33 -0900

    Corrected syntax error in users model

--- models/users-model.js
+++ models/users-model.js
@@ -10,12 +10,12 @@ function findBy(filter) {
   return db("users").where(filter)
 }
 
-function findById(id)
+function findById(id) {
   return db("users")
     .select("id", "username")
     .where({ id })
     .first();
-)
+}
 
 async function add(user) {
   const [id] = await db("users").insert(user);
